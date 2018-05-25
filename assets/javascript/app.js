bbb12ba12d252279f2b8ec2d173db934a0ae2328
Develanton <develanton@gmail.com>
2018-05-25 18:50:18 -0500

    cant get to run the app file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,22 @@
-
-
-
+ 
+ 
+ 
  //   var baseGifs = ["Star Wars","Lord of the Rings","Harry Potter","Back to the Future"];
+ $(document).ready(function() {
 
-      var butts = document.getElementById('butts').innerHTML = "hi";
-
-   
-
-      
-
-    
-
-
-
-        console.log("it's alive!");
-
-
+    var baseGifs = ["Star Wars", "Lord of the Rings", "Harry Potter", "Back to the Future"];
+    var butts = $("#butts");
+    var search = $("#search");
+  
+    baseGifs.forEach(baseGifs => {
+        butts.append("<a class='button' id='butts'>" + baseGifs + "</a>");
+  
+      //document.getElementById('butts').innerHTML = "hi";
+      $("#butts").html("hi");
+  
+    });
+    console.log("it's alive!");
+  });
 
 
 
