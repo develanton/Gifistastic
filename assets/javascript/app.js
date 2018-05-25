59b612e66fee9cc088375b91ab3d76fa353becc8
Develanton <develanton@gmail.com>
2018-05-25 18:14:42 -0500

    basic structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+
+
+
+ //   var baseGifs = ["Star Wars","Lord of the Rings","Harry Potter","Back to the Future"];
+
+      var butts = document.getElementById('butts').innerHTML = "hi";
+
+   
+
+      
+
+    
+
+
+
+        console.log("it's alive!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
