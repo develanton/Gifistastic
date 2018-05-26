1b253d4a2ef61d21704bd065aaadf1725e57fcb8
Develanton <develanton@gmail.com>
2018-05-26 00:05:03 -0500

    search button add new topic and once click on the topic button returns successfully

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,84 @@
- 
- 
- 
- //   var baseGifs = ["Star Wars","Lord of the Rings","Harry Potter","Back to the Future"];
- $(document).ready(function() {
+$(document).ready(function () {
+
 
     var baseGifs = ["Star Wars", "Lord of the Rings", "Harry Potter", "Back to the Future"];
     var butts = $("#butts");
     var search = $("#search");
-  
-    baseGifs.forEach(baseGifs => {
-        butts.append("<a class='button' id='butts'>" + baseGifs + "</a>");
-  
-      //document.getElementById('butts').innerHTML = "hi";
-      $("#butts").html("hi");
-  
+    var count = 0;
+
+    var wordloop = baseGifs.forEach(baseGifs => {
+        butts.append("<button class='button' id='butts'>" + baseGifs + "</a>");
+
     });
+
+    $("#searchButt").on("click", function (event) {
+        event.preventDefault();
+        var userInput = $("#search").val().trim();
+
+        butts.append("<button class='button' id='butts'>" + userInput + "</button>");
+
+        console.log(userInput);
+
+        $("#search").val("");
+
+    })
+
+
     console.log("it's alive!");
-  });
+
+    $(document.body).on("click", "#butts", function (event) {
+
+        var term = event.target.innerText;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=aOBNvDAUoSCFk4afN29oBolqLxw9lcaC&q=" + term + "&limit=10&offset=0&rating=R&lang=en"
+      
+        console.log("Term: " + term);
+
+
+
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+
+            var result = response.data;
+            console.log(result);
+
+            for (let i = 0; i < result.length; i++) {
+
+               
+                
+                var divers = $("#gifDisplay").append("<div>");
+
+                var ratings = ("Rating:"+ result[i].rating);
+               
+                
+              
+                var imgs = ("<img src='"+result[i].images.fixed_width.url+"'>");
+
+
+                divers.append(imgs + ratings);
+                
+
+                
+               
+                
+            }
+            
+           
+
+               
+        
+               
+        })
+
+    })
+
+
+
+
+
+});
 
 
 
